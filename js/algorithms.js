442b9164e4ac02899aa32336c9bbf8b243abd75d
elvis-egle <ee11040@lu.lv>
2017-04-01 21:27:32 +0300

    Add files via upload

--- js/algorithms.js
+++ js/algorithms.js
@@ -3,7 +3,7 @@ function fcfs(arr) {
 	return result;
 }
 
-function sstf(start, arr) {
+function sstf(start, direction, arr) {
     var result = [];
     var ordered = arr.slice();
     ordered.sort(function (a, b) {
@@ -27,7 +27,7 @@ function sstf(start, arr) {
             result.push(ordered[ordered.length-1]);
             ordered.splice(ordered.length-1,1);
         }
-        else if ((prev-ordered[i-1].pos)<(ordered[i].pos-prev)) {
+        else if ((prev-ordered[i-1].pos)<=(ordered[i].pos-prev-direction)) {
             result.push(ordered[i-1]);
             prev = ordered[i-1].pos;
             ordered.splice(i-1,1);
@@ -42,10 +42,213 @@ function sstf(start, arr) {
     return result;
 }
 
+function scan(start, size, direction, arr) {
+    var result = [];
+    var ordered = arr.slice();
+    ordered.sort(function (a, b) {
+        return a.pos - b.pos;
+    });
+    var i=0;
+    for (var index in arr) {
+        if (start<ordered[index].pos)
+        {
+            break;
+        }
+        i++;
+    }
+    i+=direction;
+    var prev = start;
+    while (ordered.length!=0) {
+        if (i<=0) {
+            if (i==0) {
+                result.push({
+                    "index": -1,
+                    "pos": 0
+                })
+                i--;
+            }
+            else {
+                result.push(ordered[0]);
+                ordered.splice(0,1);
+            }
+        }
+        else if (i>=ordered.length) {
+            result.push(ordered[ordered.length-1]);
+            if (i==ordered.length) {
+                result.push({
+                    "index": -1,
+                    "pos": size-1
+                })
+            }
+            ordered.splice(ordered.length-1,1);
+        }
+        else { //if ((prev-ordered[i-1].pos)<(ordered[i].pos-prev)) 
+            result.push(ordered[i-1]);
+            prev = ordered[i-1].pos;
+            ordered.splice(i-1,1);
+            i += direction-1;
+        }
+/*        else {
+            result.push(ordered[i]);
+            prev = ordered[i].pos;
+            ordered.splice(i,1);
+        }*/
+    }
+    return result;
+}
+
+function cscan(start, size, direction, arr) {
+    var result = [];
+    var ordered = arr.slice();
+    ordered.sort(function (a, b) {
+        return a.pos - b.pos;
+    });
+    var i=0;
+    for (var index in arr) {
+        if (start<ordered[index].pos)
+        {
+            break;
+        }
+        i++;
+    }
+    i+=direction;
+    var prev = start;
+    while (ordered.length!=0) {
+        if (i<=0) {
+            if (i==0) {
+                result.push({
+                    "index": -1,
+                    "pos": 0
+                })
+                if (!direction) i=ordered.length+1; else i--;
+            }
+            else {
+                result.push(ordered[0]);
+                ordered.splice(0,1);
+            }
+        }
+        else if (i>=ordered.length) {
+            if (i!=ordered.length+1) {
+                result.push(ordered[ordered.length-1]);
+                ordered.splice(ordered.length-1,1);
+            }
+            else {
+                result.push({
+                    "index": -1,
+                    "pos": size-1
+                })
+                if (direction) i=0; else i++;
+            }
+        }
+        else { //if ((prev-ordered[i-1].pos)<(ordered[i].pos-prev)) 
+            result.push(ordered[i-1]);
+            prev = ordered[i-1].pos;
+            ordered.splice(i-1,1);
+            i += direction-1;
+        }
+/*        else {
+            result.push(ordered[i]);
+            prev = ordered[i].pos;
+            ordered.splice(i,1);
+        }*/
+    }
+    return result;
+}
+
+function look(start, size, direction, arr) {
+    var result = [];
+    var ordered = arr.slice();
+    ordered.sort(function (a, b) {
+        return a.pos - b.pos;
+    });
+    var i=0;
+    for (var index in arr) {
+        if (start<ordered[index].pos)
+        {
+            break;
+        }
+        i++;
+    }
+    i+=direction;
+    var prev = start;
+    while (ordered.length!=0) {
+        if (i<=0) {
+            result.push(ordered[0]);
+            ordered.splice(0,1);
+        }
+        else if (i>=ordered.length) {
+            result.push(ordered[ordered.length-1]);
+            ordered.splice(ordered.length-1,1);
+        }
+        else { //if ((prev-ordered[i-1].pos)<(ordered[i].pos-prev)) 
+            result.push(ordered[i-1]);
+            prev = ordered[i-1].pos;
+            ordered.splice(i-1,1);
+            i += direction-1;
+        }
+/*        else {
+            result.push(ordered[i]);
+            prev = ordered[i].pos;
+            ordered.splice(i,1);
+        }*/
+    }
+    return result;
+}
+
+function clook(start, size, direction, arr) {
+    var result = [];
+    var ordered = arr.slice();
+    ordered.sort(function (a, b) {
+        return a.pos - b.pos;
+    });
+    var i=0;
+    for (var index in arr) {
+        if (start<ordered[index].pos)
+        {
+            break;
+        }
+        i++;
+    }
+    i+=direction;
+    var prev = start;
+    while (ordered.length!=0) {
+        if (i<=0) {
+            if (i==0) {
+                if (!direction) i=ordered.length+1; else i--;
+            }
+            else {
+                result.push(ordered[0]);
+                ordered.splice(0,1);
+            }
+        }
+        else if (i>=ordered.length) {
+            if (i!=ordered.length+1) {
+                result.push(ordered[ordered.length-1]);
+                ordered.splice(ordered.length-1,1);
+            }
+            else {
+                 if (direction) i=0; else i++;
+            }
+        }
+        else { //if ((prev-ordered[i-1].pos)<(ordered[i].pos-prev)) 
+            result.push(ordered[i-1]);
+            prev = ordered[i-1].pos;
+            ordered.splice(i-1,1);
+            i += direction-1;
+        }
+/*        else {
+            result.push(ordered[i]);
+            prev = ordered[i].pos;
+            ordered.splice(i,1);
+        }*/
+    }
+    return result;
+}
+
 var algos = {
 	fcfs: {
 		name: 'First Come-First Serve (FCFS)',
-		func: function (track_start, track_size, seek_queue) {
+		func: function (track_start, track_size, track_direction, seek_queue) {
 			var result = fcfs(seek_queue);
 			return result;
 		}
@@ -53,7 +256,7 @@ var algos = {
 	
 	/*dummy: {
 		name: 'Really dumb algo',
-		func: function (track_start, track_size, seek_queue) {
+		func: function (track_start, track_size, track_direction, seek_queue) {
 			var result = yolo_copy(seek_queue);
 			result.sort(function (a, b) {
 				return a.pos - b.pos;
@@ -64,37 +267,41 @@ var algos = {
 	
 	sstf: {
 		name: 'Shortest Seek Time First (SSTF)',
-		func: function (track_start, track_size, seek_queue) {
-			var result = sstf(track_start, seek_queue);
+		func: function (track_start, track_size, track_direction, seek_queue) {
+			var result = sstf(track_start, track_direction, seek_queue);
             return result;
 		}
 	},
 	
 	scan: {
 		name: 'Elevator (SCAN)',
-		func: function (track_start, track_size, seek_queue) {
-			return null;
+		func: function (track_start, track_size, track_direction, seek_queue) {
+			var result = scan(track_start, track_size, track_direction, seek_queue);
+            return result;
 		}
 	},
 	
 	cscan: {
 		name: 'Circular SCAN (C-SCAN)',
-		func: function (track_start, track_size, seek_queue) {
-			return null;
+		func: function (track_start, track_size, track_direction, seek_queue) {
+			var result = cscan(track_start, track_size, track_direction, seek_queue);
+            return result;
 		}
 	},
 	
 	look: {
 		name: 'LOOK',
-		func: function (track_start, track_size, seek_queue) {
-			return null;
+		func: function (track_start, track_size, track_direction, seek_queue) {
+			var result = look(track_start, track_size, track_direction, seek_queue);
+            return result;
 		}
 	},
 	
 	clook: {
 		name: 'C-LOOK',
-		func: function (track_start, track_size, seek_queue) {
-			return null;
+		func: function (track_start, track_size, track_direction, seek_queue) {
+			var result = clook(track_start, track_size, track_direction, seek_queue);
+            return result;
 		}
 	}
 };
